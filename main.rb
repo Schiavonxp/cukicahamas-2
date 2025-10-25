ed5dbfb7e5e6abfe35ba9515e50fb8201cff9f27
Schiavonxp <victor.schiavon@hotmail.com>
2025-10-25 18:34:20 +0000

    Initial commit: ruby project

---
+++ main.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# cukicahamas-2 - Ruby Implementation
+# A simple Hello World program
+
+def main
+  puts "Hello, World!"
+  puts "Welcome to cukicahamas-2"
+  
+  # Simple calculation
+  numbers = [1, 2, 3, 4, 5]
+  total = numbers.sum
+  puts "Sum of numbers: #{total}"
+end
+
+main
